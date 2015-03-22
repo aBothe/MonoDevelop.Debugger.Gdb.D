15dee282bfb344263a40f22dd6da9a7c96e3630a
Alexander Bothe <info@alexanderbothe.com>
2015-03-22 22:32:16 +0100

    Adapted stuff to new D_Parser API

--- Misc/MemberLookup.cs
+++ Misc/MemberLookup.cs
@@ -44,7 +44,7 @@ namespace MonoDevelop.Debugger.Gdb.D
 		protected static KeyValuePair<TemplateIntermediateType, MemberSymbol[]>[] GetMembers(TemplateIntermediateType ct, ResolutionContext ctxt)
 		{
 			var lk = new MemberLookup(ctxt);
-			lk.DeepScanClass (ct, MemberFilter.Variables, false);
+			lk.DeepScanClass (ct, new ItemCheckParameters(MemberFilter.Variables) { resolvedCurScope = ct }, false);
 
 			var res = new List<KeyValuePair<TemplateIntermediateType, MemberSymbol[]>>();
 			var l = new List<MemberSymbol> ();
@@ -61,38 +61,16 @@ namespace MonoDevelop.Debugger.Gdb.D
 			return res.ToArray();
 		}
 
-		protected override bool HandleItem(PackageSymbol pack)
+		protected override void HandleItem (INode n, AbstractType resolvedCurrentScope)
 		{
-			return false;
+			//TODO: Mixins & template mixins - their mixed-in var definitions are handled _after_ the actual definition.
+			tempMembers.Add(TypeDeclarationResolver.HandleNodeMatch(n, ctxt, resolvedCurrentScope) as MemberSymbol);
 		}
 
-		protected override bool HandleItem(INode n)
+		protected override bool PreCheckItem (INode n)
 		{
 			var dv = n as DVariable;
-			if (dv != null && !dv.IsAlias && !dv.IsStatic)
-			{
-				//TODO: Mixins & template mixins - their mixed-in var definitions are handled _after_ the actual definition.
-				tempMembers.Add(TypeDeclarationResolver.HandleNodeMatch(dv, ctxt, TemporaryResolvedNodeParent) as MemberSymbol);
-			}
-			return false;
-		}
-
-		public override IEnumerable<DModule> PrefilterSubnodes(ModulePackage pack, out ModulePackage[] subPackages)
-		{
-			subPackages = null;
-			return null;
-			//return base.PrefilterSubnodes(pack, out subPackages);
-		}
-
-		public override IEnumerable<INode> PrefilterSubnodes(IBlockNode bn)
-		{
-			var vars = new List<INode>();
-			foreach (var n in bn)
-				if (n is DVariable && !(n as DVariable).IsAlias)
-					vars.Add(n);
-			if (vars.Count == 0)
-				return null;
-			return vars;
+			return dv != null && !dv.IsAlias && !dv.IsStatic;
 		}
 	}
 }
